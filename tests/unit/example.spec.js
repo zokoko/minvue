3f3a7e6ff8742ed064f796f6a00145972dd0fb19
zokoko <zokoko@163.com>
2020-11-26 10:05:00 +0800

    ut time infotext

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,24 +1,53 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
 
-  const msg = 'new message'    
+  const msg = 'new message'
 
-  const wrapper0 = shallowMount(HelloWorld, {
-    props: { msg }
+  const wrapper0 = mount(HelloWorld, {
+    props: {
+      msg
+    }
   })
 
   it('renders props.msg when passed', () => {
-    
+
     expect(wrapper0.text()).toMatch(msg)
 
   })
 
-  const wrapper = shallowMount(HelloWorld)
+  const wrapper = mount(HelloWorld)
   it('renders the correct infomation', () => {
-    expect(wrapper.html()).toContain('<div class="gitInfoText"></div>')})
+    expect(wrapper.html()).toContain('<div class="gitInfoText"></div>')
+  })
+
+  // it('has a stop button', () => {
+  //   expect(wrapper.contains('button')).toBe(true)})
+  var that = this
+  test('time props', async () => {
+    const time = 1000
+    const wrapper = mount(HelloWorld, {
+      props: {
+        time
+      }
+    })
+  
+    await wrapper.setProps({ time: 3000 })
+  
+    expect(wrapper.vm.time).toBe(3000)
+  })
 
 
+  it('gitInfoText int', () => {
+    expect(wrapper.vm.gitInfoText).toBe('')
+  })
+
+
+  
+  
+  
+
+  
 
 })
