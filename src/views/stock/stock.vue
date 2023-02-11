9c99a3f905ba7f211d57895953dc6960d0f514c5
zokoko <zokoko@163.com>
2023-02-12 07:50:56 +0800

    httpservices调用get post

--- src/views/stock/stock.vue
+++ src/views/stock/stock.vue
@@ -8,19 +8,39 @@
 
 <script>
 import axios from 'axios'
+import httpservices from '@/services/httpservices'
 export default {
   name: 'Stock',
   methods: {
     add: function () {
       console.log(this)
       this.$store.dispatch('module1/add')
-      axios({
-        method: 'post',
-        url: '/api/luyou/post',
-        data: {
-          date1: '2020-09-07'
-        }
-      }).then((res) => console.log(res))
+      let url = 'https://www.baidu.com'
+      httpservices
+        .get(url, { page: 1 })
+        .then((response) => {
+          console.log('GET 响应：', response)
+        })
+        .catch((error) => {
+          console.log('GET 错误：', error)
+        })
+
+      httpservices
+        .post(url, { name: 'Alice' })
+        .then((response) => {
+          console.log('POST 响应：', response)
+        })
+        .catch((error) => {
+          console.log('POST 错误：', error)
+        })
+
+      // axios({
+      //   method: 'post',
+      //   url: '/api/luyou/post',
+      //   data: {
+      //     date1: '2020-09-07'
+      //   }
+      // }).then((res) => console.log(res))
       console.log('submit!')
     }
   },
